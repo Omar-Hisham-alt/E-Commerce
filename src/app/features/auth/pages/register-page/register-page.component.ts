684ab8d30219d79313e99913d61d5e5459da7e03
Omar-Hisham-alt <omarhisham5580@gmail.com>
2025-12-18 16:41:38 +0200

    Omar

--- src/app/features/auth/pages/register-page/register-page.component.ts
+++ src/app/features/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,26 @@
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
+  /*
 
+  "name": "Route",
+  "email":"ahmedmuttii4012@gmail.com",
+  "password":"Ahmed@123",
+  "rePassword":"Ahmed@123",
+  "phone":"01010700701"
+
+   */
+
+  userEmail = new FormControl(null, [Validators.required, Validators.minLength(5)]);
+
+  submitData(): void {
+    console.log(this.userEmail);
+  }
 }
